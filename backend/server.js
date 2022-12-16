360e637ffd2740b0f61de4d44f3d47d43b77dfa1
yuching1128 <yuching1128@gmail.com>
2022-12-15 23:40:41 -0500

    Node API Setup

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,32 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose')
 require('dotenv').config();
+// bring routes
+const blogRoutes = require('./routes/blog');
 
 // app
 const app = express();
+
+// database
+mongoose.set('strictQuery', true);
+mongoose
+    .connect(process.env.DATABASE_LOCAL, {})
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("DB Error => ", err));
 
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 //cors
-app.use(cors());
+if(process.env.NODE_ENV == 'development'){
+    app.use(cors({origon: `${process.env.CLIENT_URL}`}));
+};
 
-// routes
-app.get('/api', (req, res) =>{
-    res.json({time: Date().toString()});
-});
+// routes middleware
+app.use('/api', blogRoutes);
 
 // port
 const port = process.env.PORT || 8000
