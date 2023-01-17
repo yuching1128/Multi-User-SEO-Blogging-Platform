7fcd80633132285c251e8a038fc557815abc1f30
yuching1128 <yuching1128@gmail.com>
2023-01-17 02:41:34 -0500

    Protected routes for Admin

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -45,9 +45,24 @@ const Header = () => {
                                 </NavItem>
                             </React.Fragment>
                         )}
+                        {isAuth() && isAuth().role === 0 && (
+                            <NavItem>
+                                <Link href="/user">
+                                    <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                                </Link>
+                            </NavItem>
+                        )}
+
+                        {isAuth() && isAuth().role === 1 && (
+                            <NavItem>
+                                <Link href="/admin">
+                                    <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                                </Link>
+                            </NavItem>
+                        )}
                         {isAuth() && (<NavItem>
-                            <NavLink style={{ cursor: 'pointer' }} onClick={() => signout(() => Router.replace(`/signin`))}>Signout</NavLink>
-                        </NavItem>
+                                <NavLink style={{ cursor: 'pointer' }} onClick={() => signout(() => Router.replace(`/signin`))}>Signout</NavLink>
+                            </NavItem>
                         )}
 
                     </Nav>
